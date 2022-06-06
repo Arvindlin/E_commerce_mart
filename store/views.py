85506e6826698deb00037e11cb62eb6740f174b2
arvindlin <arvind.lingwal@gmail.com>
2022-06-06 07:19:06 +0530

    model and templete added.

--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
+def all_products(request):
+    """View to get/Retrive the data from the database."""
+    product = Product.objects.all()
+    return render(request, 'store/home.html', {'products': product})
