ac13ec7ff0b97de787269dc84416e89eb67c5158
arvindlin <arvind.lingwal@gmail.com>
2022-06-12 13:35:35 +0530

    product view and templets added

--- store/views.py
+++ store/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
-
 
 def categories(request):
     return {
@@ -11,4 +10,8 @@ def categories(request):
 def all_products(request):
     """View to get/Retrive the data from the database."""
     product = Product.objects.all()
-    return render(request, 'store/home.html', {'products': product})
+    return render(request, 'store/home.html', {'product': product})
+
+def product_details(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/products/details.html', {'product': product})
